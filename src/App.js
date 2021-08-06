68a3f80a5ba0316bfef8bd69b8cf8fd19f3732ed
jsonthany <jason.thany@outlook.com>
2021-08-07 00:35:43 +0800

    added pagination to the search page. this template can be used for the main menu page as well.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,48 @@ import Tinder from './components/Tinder/Tinder'
 
 import { useState, useEffect } from 'react';
 
-const FEATURED_API = "https://api.themoviedb.org/3/movie/popular?api_key=a52ad5ea9ff27be0460bfc281fe3cbd7&language=en-US&page=1";
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=a52ad5ea9ff27be0460bfc281fe3cbd7&query=";
-
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [moviesRaw, setMoviesRaw] = useState([]);
+  const [search, setSearch] = useState('');
+  let pageNumber = 1;
+  let maxPage = 0;
+
+  let FEATURED_API = `https://api.themoviedb.org/3/movie/popular?api_key=a52ad5ea9ff27be0460bfc281fe3cbd7&language=en-US`;
+  let SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=a52ad5ea9ff27be0460bfc281fe3cbd7&api_key=API_KEY&query=`;
 
   useEffect (() => {
     getMovie(FEATURED_API);
   }, []);
 
   const getMovie = (API) => {
-    fetch(API)
+    fetch(API + '&page=' + pageNumber)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         setMovies(data.results);
+        setMoviesRaw(data);
+        maxPage=data.total_pages;
       });
   }
 
   // Search for movies in the database
-  const onSubmitHandler = async (search) => {
+  const onSubmitHandler = (search) => {
+    if (search) {
+      pageNumber = 1;
+      setSearch(search);
+      getMovie(SEARCH_API + search);
+    } else {
+      pageNumber = 1;
+      setSearch('');
+      getMovie(FEATURED_API);
+    }
+  }
+
+  const changePageNumber = (page) => {
+    pageNumber = page;
+    
     if (search) {
       getMovie(SEARCH_API + search);
     } else {
@@ -36,9 +57,12 @@ function App() {
     <>
       {/* <Header /> */}
       <Navbar />
-      {/* <Search searchfn={ onSubmitHandler }
-              movies={ movies } /> */}
-      <Tinder movies={ movies }/>
+      <Search searchfn={ onSubmitHandler }
+              movies={ movies }
+              changePageNumber={ changePageNumber }
+              pageNumber={pageNumber}
+              moviesRaw={moviesRaw} />
+      {/* <Tinder movies={ movies }/> */}
     </>
   );
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,29 +1,47 @@
 import SearchBar from './SearchBar'
 import Movies from './Movies'
-import { Row, Layout } from 'antd';
+import { Row, Layout, Pagination } from 'antd';
 import './Search.css';
 
 
-const Search = ({ searchfn, movies }) => {
+const Search = ({ searchfn, movies, changePageNumber, pageNumber, moviesRaw }) => {
 
     const { Header, Footer, Content } = Layout;
+
+    let num = 1;
+
+    const onChange = (pageNum) => {
+        changePageNumber(pageNum);
+    }
+
+    const getTotalPages = () => {
+        return moviesRaw.total_pages * 10;
+    }
+
+    const getCurrentPage = () => {
+        return moviesRaw.page;
+    }
 
     return (
         <Layout className="search">
             <Header className="searchbar">
                 <SearchBar  className="searchbar"
                             searchfn={ searchfn }
+                            // resetPageNumber={ resetPageNumber }
                             style={{ marginBottom: 100 }} />
             </Header>
             <Content className="content">
-                <div>
-                    <Row justify="center">
-                        {movies.map(movie =>
-                            <Movies className='movies_container'
-                                    key={movie.id} {...movie} />
-                        )}
-                    </Row>
-                </div>
+                <Row justify="space-around">
+                    {movies.map(movie =>
+                        <Movies className='movies_container'
+                                key={movie.id} {...movie} />
+                    )}
+                </Row>
+                <br />
+                <br />
+                <Row justify="center">
+                <Pagination defaultCurrent={1} pageSize={10} showSizeChanger={false} showcurrent={getCurrentPage()} total={getTotalPages()} onChange={onChange}/>
+                </Row>
             </Content>
             <Footer className="footer">
                 RandomMovieGenerator @2021 / Search
